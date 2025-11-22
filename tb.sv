746caa4683ac000861cae656953640e09bf7d1d0
jcur02 <jcur1802@gmail.com>
2025-11-21 20:58:07 -0600

    Add testbench

---
+++ tb.sv
@@ -0,0 +1,29 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
+module tb;
+  reg clk;
+  
+  always #10 clk =~ clk;
+  mesh_gnrt_if 	_if (clk);
+  mesh_gnrtr u0 ( 	.clk(clk),
+             .pndng(_if.pndng),
+             .data_out(_if.data_out),
+             .popin (_if.popin),
+             .pop(_if.pop),
+             .data_out_i_in(_if.data_out_i_in),
+             .pndng_i_in(_if.pndng_i_in),
+             .reset(_if.reset));
+  trans_test t0;
+  
+  initial begin
+    clk <= 0;
+    uvm_config_db#(virtual mesh_gnrt_if)::set(null, "uvm_test_top", "vif", _if);
+    run_test("trans_test");
+  end
+  
+  initial begin
+    $dumpvars;
+    $dumpfile ("dump.vcd");
+  end
+endmodule
