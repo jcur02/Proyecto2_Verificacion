26432cf4043ca7bdcc15e3b2221a2934f0d7ad1e
jcur02 <jcur1802@gmail.com>
2025-11-25 16:25:01 -0600

    Update sequence

--- Sequence/gen_sequence.sv
+++ Sequence/gen_sequence.sv
@@ -3,26 +3,27 @@ import uvm_pkg::*;
 
 class gen_sequence extends uvm_sequence;
   `uvm_object_utils(gen_sequence)
-
-    // constructor
   function new(string name = "gen_sequence");
     super.new(name);
   endfunction
 
-  // cantidad de transacciones a generar  
-  rand int num;
-  
-  constraint c1 { num inside {[3:4]}; }
+  rand int num_per_terminal[16];
+
+  // cantidad de transacciones por terminal
+  constraint c_counts {
+    foreach (num_per_terminal[i]) num_per_terminal[i] inside {[3:10]};
+  }
 
   virtual task body();
-    for (int i = 0; i < num; i ++) begin
-    	trans_item m_item = trans_item::type_id::create("m_item");
-    	start_item(m_item);
-    	m_item.randomize();
-    	`uvm_info("SEQ", $sformatf("Generate new item: "), UVM_LOW)
-    	m_item.print(); // imprimir la transaccion generada
-      	finish_item(m_item);
+    int t;
+    foreach (num_per_terminal[t]) begin
+      for (int k = 0; k < num_per_terminal[t]; k++) begin
+        trans_item m_item = trans_item::type_id::create($sformatf("m_item_%0d_%0d", t, k));
+        start_item(m_item);
+        assert(m_item.randomize() with { sender == t; });
+        `uvm_info("SEQ", $sformatf("Generate item from terminal %0d with gap of %0d", t, m_item.send_gap), UVM_LOW)
+        finish_item(m_item);
+      end
     end
-    `uvm_info("SEQ", $sformatf("Done generation of %0d items", num), UVM_LOW)
   endtask
 endclass
